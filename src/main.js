48a7008ea2eee8c4b6755503cafd01832d25ca5a
Yu Wu <ywu928@uwo.ca>
2020-12-02 16:40:16 -0500

    ViewUI toolkit updated

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store/index.js'
 import './assets/lib/flexible.js'
 import './assets/lib/Chart'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
 /**使用vux的插件提示功能*/
 import  { ToastPlugin } from 'vux'
 Vue.use(ToastPlugin)
-
+Vue.use(ViewUI)
 Vue.config.productionTip = false
 
 new Vue({
